efa37651a1a3fb41b51fdcec90efe843075423dc
AKHIL PIOUS <akhilpioussince2001@gmail.com>
2026-04-21 11:04:36 +0530

    Implemented group-based messaging channel layers with broadcast functionality

--- realtime_chat/chat/consumers.py
+++ realtime_chat/chat/consumers.py
@@ -1,20 +1,40 @@
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 import json
 
 
 class ChatConsumer(WebsocketConsumer):
 
     def connect(self):
+        self.room_name = "global"
+        self.room_group_name = f"chat_{self.room_name}"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
         self.accept()
-        print("WebSocket Connected")
 
     def disconnect(self, close_code):
-        print("WebSocket Disconnected")
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
 
     def receive(self, text_data):
         data = json.loads(text_data)
-
         message = data["message"]
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message
+            }
+        )
+
+    def chat_message(self, event):
+        message = event["message"]
 
         self.send(text_data=json.dumps({
             "message": message
